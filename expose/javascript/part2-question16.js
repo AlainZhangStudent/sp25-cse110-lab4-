1e428ea00ac9ccbe41f0991e18c824563fb4e0e5
AlainZhangStudent <156254247+AlainZhangStudent@users.noreply.github.com>
2025-04-29 03:15:04 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,14 @@
+let statistics = {
+    redCards: 21,
+    blueCards: 45,
+    greenCards: 12,
+    raceCars: 5,
+    blackCards: 40,
+    rareCars: 2
+};
 
+for (let property in statistics) {
+    if (property.startsWith('r') || statistics[property] % 2 !== 0) {
+        console.log(statistics[property]);
+    }
+}
